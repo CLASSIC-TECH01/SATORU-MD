2968365b09e828e734cc95cdf0200f82570f68a0
DevNinja <mohsani12017@gmail.com>
2024-05-31 19:17:25 +0100

    Update channel.js

--- plugins/channel.js
+++ plugins/channel.js
@@ -1,15 +1,15 @@
 let handler = async m => m.reply(`
 
 ╭⭑⭑⭑★✪•••*ՏͲᎪᎡ*•••✪★⭑⭑⭑
-│ 📂 *BOT Name:* _ՏͲᎪᎡ-ᎷᎠ-Ꮩ2_
-│ 📝 *Description:* _I'm STAR-MD-V2. A  
-┃MultiDevice WhatsApp bot with rich features  ┃Created BY +2347045035241._
-│ 👤 *Owner:* _乇乂匚乇ㄥ_
-│ 🌐 *Channel:* https://whatsapp.com/channel/0029VaBcXo4JJhzW9c1uVD2X
+│ 📂 *BOT Name:* _SATORU-MD_
+│ 📝 *Description:* _I'm SATORU-MD. A  
+┃MultiDevice WhatsApp bot with rich features  ┃Created BY +2349012316690._
+│ 👤 *Owner:* _VABUJEE
+│ 🌐 *Channel:* https://whatsapp.com/channel/0029VagceWnLtOjCyj005L0A
 ╰━━━━━━━━━━━━━━━━╯
 `.trim())
 handler.help = ['channel']
 handler.tags = ['main']
-handler.command = ['star', 'channel' ] 
+handler.command = ['SATORU', 'channel' ] 
 
 export default handler
